60651b2a1aa62468ab957d47473dcff21c88f885
daataguy <59241099+daataguy@users.noreply.github.com>
2020-01-31 17:39:41 -0500

    Black_Scholes
    european options pricing (calls,puts)

---
+++ Scholes_Options_Pricing.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 17:26:02 2020
+
+@author: jeremiah
+"""
+
+import numpy as np
+import scipy.stats as si
+import sympy as sy
+import sympy.stats as systats
+
+#Criteria of European call/put 
+    #exercised at expiration
+    #no dividends
+    #ambiguous market
+    #risk-free-interest-rate delta=0
+    #lognormal distribution(log of norm dist is normally distributed)
+    
+#european call
+
+def euro_vanilla_call(S, K, T, r, sigma):
+    
+    #S: spot price
+    #K: strike price
+    #T: time to maturity
+    #r: interest rate
+    #sigma: volatility of underlying asset (standard dev of asset returns)
+    
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    
+    return call
+
+#eurpoean put
+
+def euro_vanilla_put(S, K, T, r, sigma):
+    
+    #S: spot price
+    #K: strike price
+    #T: time to maturity
+    #r: interest rate
+    #sigma: volatility of underlying asset
+    
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    
+    put = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+    
+    return put
+
+#indicate call or option
+
+def euro_vanilla(S, K, T, r, sigma, option = 'call'):
+    
+    #S: spot price
+    #K: strike price
+    #T: time to maturity
+    #r: interest rate
+    #sigma: volatility of underlying asset
+    
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    
+    if option == 'call':
+        result = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    if option == 'put':
+        result = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+        
+    return result
